738aec849c0240f5a3c12e8818107583649f5cb8
vergilgxw <vergilgxw@gmail.com>
2014-11-12 23:19:57 +0800

    modify datagen, and write a main func

---
+++ main.m
@@ -0,0 +1,16 @@
+% This is the main function of this project
+
+% Setting hyperparameters
+m_mu = 0;
+m_mv = 0;
+v_mu = 1;
+v_mv = 1;
+
+a0p = 10/2;
+b0p = 10/2;
+
+a0 = 10/2;
+b0 = 10*sqrt(10) /2;
+
+m_b0 = [-6; -2; 2; 6];
+v0 = 0.1;
