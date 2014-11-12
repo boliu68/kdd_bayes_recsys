dce1bcc91714ac2fbf3dd7c9b9a558649b59c806
vergilgxw <vergilgxw@gmail.com>
2014-11-12 23:23:11 +0800

    modify main

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 % This is the main function of this project
 
-% Setting hyperparameters
+%% Setting hyperparameters
 m_mu = 0;
 m_mv = 0;
 v_mu = 1;
@@ -14,3 +14,18 @@ b0 = 10*sqrt(10) /2;
 
 m_b0 = [-6; -2; 2; 6];
 v0 = 0.1;
+
+max_iter = 100;
+
+%% Initialization should be here
+
+
+
+% This is the iteration of update
+for iter = 1:max_iter
+    
+    
+    
+    
+    
+end
