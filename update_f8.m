4fdccc01a8ec369b84c74ce8ccdc177f9f219af4
Bo Liu <6liubo8@gmail.com>
2014-11-13 02:46:39 +0800

    4 left

--- update_f8.m
+++ update_f8.m
@@ -1,6 +1,25 @@
 function para = update_f8(para, hyperpara)
 
     %k = 1 for L, j = 1 to d
-    v_k_b0_n8j = 
+    %L * d
+    for j = 1:d
+        %L dimension vector
+        v_b0_n8j = (1 ./ ((1 ./ para.v_b0) - (1 ./ para.h_v_b08j)));
+        m_b0_n8j = v_b0_n8j .* ((para.m_b0 .* (1 ./ para.v_b0) - para.h_m_b08j .* (1 ./ para.h_v_b08j)));
 
+        v_b_n8j(j,:) = (1 ./ ((1 ./ para.v_b(j,:)) - (1 ./ para.h_v_b8j(j,:))));
+        m_b_n8j(j,:) = v_b_n8j(j,:) .* (para.m_b(j,:)  .* (1 ./ para.v_b(j,:)) - para.h_m_b8j(j,:) .* (1 ./ para.h_v_b8j(j,:)));
+
+        para.h_m_b08j = m_b_n8j(j,:);
+        para.h_v_b08j = v_b_n8j(j,:) + para.v0;
+        para.h_m_b8j = m_b0_n8j;
+        para.h_v_b8j = v_b0_n8j + para.v0;
+
+        para.v_b0 = (1 ./ ((1 ./ v_b0_n8j) + (1 ./ para.h_v_b08j)));
+        para.m_b0 = para.v_b0 .* (m_b0_n8j .* (1 ./ v_b0_n8j) + para.h_m_b08j .* (1 ./ para.h_v_b08j));
+        
+        para.v_b(j,:) = (1 ./ ((1 ./ v_b_n8j(j,:)) + (1 ./ para.h_v_b8j(j,:))));
+        para.m_b(j,:) = para.v_b(j,:) .* (m_b_n8j(j,:) .* (1 ./ v_b_n8j(j,:)) + para.h_m_b8j(j,:) .* (1 ./ para.h_v_b8j(j,:)));
+    end
+    
 end
---
+++ update_f9.m
@@ -0,0 +1,54 @@
+function para = update_f9(para, hyperpara)
+
+    d = size(para.h_v_v, 1);
+
+    for j = 1:d
+           %h dimension
+            v_mV_n9jk = (1 ./ ((1 ./ para.v_mV) - (1 ./ para.h_v_mV9jk)));
+            m_mV_n9jk = v_mV_n9jk .* (para.m_mV .* (1 ./ para.v_mV) - para.h_m_mV9jk .* (1 ./ para.h_v_mV9jk));
+            
+            %d * h matrix
+            v_v_n9jk(j,:) = (1 ./ ((1 ./ para.v_v(j,:)) - (1 ./ para.h_v_v9jk(j,:))));
+            m_v_n9jk(j,:) = v_v_n9jk(j,:) .* (para.m_v(j,:) .* (1 ./ para.v_v(j,:)) - para.h_m_v9jk(j,:) .* (1 ./ para.h_v_v9jk(j,:)));
+            
+            %h dimension
+            a_vV_n9jk = (para.a_vV - para.h_a_vV9jk + 1);
+            b_vV_n9jk = (para.b_vV - para.h_b_vV9jk);
+            
+            is_update = ( b_vV_n9jk > 0) & ((2 * a_vV_n9jk - 2) > 0) & (v_mV_n9jk > 0) & (v_v_n9jk(j,:) > 0) 
+            
+            %normalization of h dimension
+            mean_val = m_v_n9jk(j,(is_update))
+            %might need sqrt here
+            std_val = v_v_n9jk(j,is_update) + v_mV_n9jk(is_update) + (2 * b_vV_n9jk(is_update)) ./ (2 * a_vV_n9jk(is_update) - 2);
+            std_val1 = v_v_n9jk(j,is_update) + v_mV_n9jk(is_update) + (2 * b_vV_n9jk(is_update)) ./ (2 * a_vV_n9jk(is_update));
+            std_val2 = v_v_n9jk(j,is_update) + v_mV_n9jk(is_update) + (2 * b_vV_n9jk(is_update)) ./ (2 * a_vV_n9jk(is_update) + 2);
+            
+            Z = pdf('Normal', m_mV_n9jk, mean_val, std_val);
+            Z1 = pdf('Normal', m_mV_n9jk, mean_val, std_val1);
+            Z2 = pdf('Normal', m_mV_n9jk, mean_val, std_val2);
+            
+            para.h_v_mV9jk(is_update) = 2 * b_vV_n9jk(is_update) ./ (2 * a_vV_n9jk(is_update) - 2) + v_v_n9jk(j,is_update);
+            para.h_m_mV9jk(is_update) = m_v_n9jk(j,is_update);
+            para.h_v_v9jk(j,is_update) = 2 * b_vV_n9jk(is_update) / (2 * a_vV_n9jk(is_update) - 2) + v_mV_n9jk(is_update);
+            para.h_m_v9jk(j,is_update) = m_mV_n9jk(is_update);
+            
+            %a dot and b dot
+            ad = (a_vV_n9jk(is_update) .* Z1 .* Z1) ./ ((a_vV_n9jk(is_update) + 1) .* Z .* Z2 - a_vV_n9jk(is_update) .* Z1 .* Z1);
+            bd = (b_vV_n9jk(is_update) .* Z .* Z1) ./ ((a_vV_n9jk(is_update) + 1) .* Z .* Z2 - a_vV_n9jk(is_update) .* Z1 .* Z1);
+            
+            para.h_a_vV9jk(is_update) = ad - a_vV_n9jk(is_update) + 1;
+            para.h_b_vV9jk(is_update) = bd - b_vV_n9jk(is_update);
+            
+            para.v_mV(is_update) = (1 ./ ((1 ./ v_mV_n9jk(is_update)) + (1 ./ para.h_v_mV9jk(is_update))));
+            para.m_mV(is_update) = para.v_mV(is_update) .* (m_mV_n9jk(is_update) ./ v_mV_n9jk(is_update) + para.h_m_mV9jk(is_update) ./ para.h_v_mV9jk(is_update));
+            
+            para.v_v(j,is_update) = (1 ./ ((1 ./ v_v_n9jk(j,is_update)) + (1 ./ para.h_v_v9jk(j,is_update))));
+            para.m_v(j,is_update) = para.v_v(j,is_update) .* (m_v_n9jk(j,is_update) ./ v_v_n9jk(j,is_update) + para.h_m_v9jk(j,is_update) ./ para.h_v_v9jk(j,is_update));
+            
+            para.a_vV(is_update) = a_vV_n9jk(is_update) + para.h_a_vV9jk(is_update) - 1;
+            para.b_vV(is_update) = b_vV_n9jk(is_update) + para.h_b_vV9jk(is_update);
+            
+    end
+
+end
