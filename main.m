1422616e4904d9d7bff8e810a85205fe4e4e9b17
vergilgxw <vergilgxw@gmail.com>
2014-11-16 13:31:31 +0800

    complete active, modify pred, waiting for test

--- main.m
+++ main.m
@@ -7,21 +7,21 @@ clc;
 %d: #Items
 %L: #Classes
 %h: The rank of matrix
-% hyperpara.n = 10;
-% hyperpara.d = 5;
-% hyperpara.L = 5;
-% hyperpara.h = 3;
-% 
-% %Test for syntax
-% R = randi(5, hyperpara.n, hyperpara.d);
-% O = randi([0,1], hyperpara.n, hyperpara.d) == 1;
-data = load('movielens.mat');
-R = data.x;
-O = full(R) > 0;
-hyperpara.n = size(R,1);
-hyperpara.d = size(R,2);
+hyperpara.n = 30;
+hyperpara.d = 20;
 hyperpara.L = 5;
-hyperpara.h = 30;
+hyperpara.h = 10;
+
+%Test for syntax
+R = randi(5, hyperpara.n, hyperpara.d);
+O = randi([0,1], hyperpara.n, hyperpara.d) == 1;
+% data = load('movielens.mat');
+% R = data.x;
+% O = full(R) > 0;
+% hyperpara.n = size(R,1);
+% hyperpara.d = size(R,2);
+% hyperpara.L = 5;
+% hyperpara.h = 30;
 
 %% Setting hyperparameters
 hyperpara.m_mu = 0;
@@ -95,7 +95,3 @@ for iter = 1:max_iter
     
 end
 
-para.v_b
-para.m_b
-para.v_a
-para.m_a
