62105a83413b7272ecedb66c450db3609241bd7e
vergilgxw <vergilgxw@gmail.com>
2014-11-17 04:04:25 +0800

    close form

--- f11_gradient_update.m
+++ f11_gradient_update.m
@@ -1,4 +1,4 @@
-function [ m_u, m_v ] = f11_gradient_update( para, hyperpara,local_para, option  )
+function [ m_u, m_v ] = f11_gradient_update( para, hyperpara,local_para, option,O  )
 %F11_GRADIENT_UPDATE Summary of this function goes here
 %   Detailed explanation goes here
 stepsize = option.stepsize;
@@ -7,23 +7,25 @@ eps = option.eps;
 
 m_u = para.m_u;
 m_v = para.m_v;
+
+stop_update = 0;
 for iter = 1: maxIter
     
  
 
-[ g_m_u, g_m_v ] = compute_f11_gradent(hyperpara,  para,O, local_para )
+[ g_m_u, g_m_v ] = compute_f11_gradent(hyperpara,  para,O, local_para );
 m_u = m_u - stepsize * g_m_u;
 m_v = m_v - stepsize * g_m_v;
 
 
 %check stop condition
-objvalue =   objective_func(para, O, local.para );
+objvalue =   objective_func(para, O, local_para );
 if(iter > 1)
-     stop = ((old_objvalue - objvalue) < eps);
+     stop_update = ((old_objvalue - objvalue) < eps);
 end
 old_objvalue = objvalue;
 
-if stop
+if stop_update ==1
     break;    
 end
 
--- main.m
+++ main.m
@@ -55,7 +55,7 @@ init_para
 % This is the iteration of update
 for iter = 1:max_iter
      disp(['Iteration:',int2str(iter)])
-
+%{
     para = update_f1(para, hyperpara);
     para = update_f2(para, hyperpara);
     para = update_f3(para, hyperpara);
@@ -66,10 +66,12 @@ for iter = 1:max_iter
     para = update_f8(para, hyperpara);
     para = update_f9(para, hyperpara);
     para = update_f10(para, hyperpara);
+     %}
     para = update_f11(para, hyperpara, O);
+    %{
     para = update_f12(para, hyperpara, O, iter);
     para = update_f13(para, hyperpara, O, R);
-
+%}
 end
 
 [pred_entry.row, pred_entry.col, ~] = find(O);
