3ec0fcf5a21c7eefa71d6a7d1fce3d1da656e5e6
vergilgxw <vergilgxw@gmail.com>
2014-11-18 13:00:16 +0800

    addded

--- experiment.m
+++ experiment.m
@@ -3,7 +3,7 @@ clear;
 
 datapath = 'movielens10k.mat';
 R = importdata(datapath);
-R = randi([0,5],500,500);
+%R = randi([0,5],500,500);
 R = sparse(R);
 O = R > 0;
 % 
@@ -13,51 +13,51 @@ d = size(R,2);
 L = 5;
 h = 10;
 
-tr_fraction = 0.2;
+tr_fraction = 0.8;
 
 %random split the Training data and test data
 [nnz_i, nnz_j, ~] = find(O);
-O_tr = sparse(zeros(500,500));%O;
+O_tr = O;
 O_tst = sparse(zeros(n,d));
 % 
-% count = 0;
-% while true
-%     
-%     row_gt2 = zeros(n,1);
-%     row_gt2(sum(O,2) > 1) = 1;
-%     col_gt2 = zeros(1,d);
-%     col_gt2(sum(O) > 1)=1;
-%     
-%     is_gt2 = sparse(((row_gt2 * col_gt2) == 1) & O_tr);
-%     
-%     if sum(is_gt2) == 0
-%        break 
-%     end
-%     
-%     [nnz_i, nnz_j, ~] = find(is_gt2);
-%     idx = randi([1,length(nnz_i)]);
-%     
-%     O_tst(nnz_i(idx), nnz_j(idx)) = 1;
-%     O_tr(nnz_i(idx), nnz_j(idx)) = 1;
-%     
-%     count = count + 1
-%     if count > floor((1 -tr_fraction) * nnz(O))
-%        break 
-%     end
-% end
+count = 0;
+while true
+    
+    row_gt2 = zeros(n,1);
+    row_gt2(sum(O,2) > 1) = 1;
+    col_gt2 = zeros(1,d);
+    col_gt2(sum(O) > 1)=1;
+    
+    is_gt2 = sparse(((row_gt2 * col_gt2) == 1) & O_tr);
+    
+    if sum(is_gt2) == 0
+       break 
+    end
+    
+    [nnz_i, nnz_j, ~] = find(is_gt2);
+    idx = randi([1,length(nnz_i)]);
+    
+    O_tst(nnz_i(idx), nnz_j(idx)) = 1;
+    O_tr(nnz_i(idx), nnz_j(idx)) = 1;
+    
+    count = count + 1
+    if count > floor((1 -tr_fraction) * nnz(O))
+       break 
+    end
+end
 
-tr_idx = randsample(nnz(O), floor(tr_fraction * nnz(O)));
-tr_id=sub2ind(size(O),nnz_i(tr_idx),nnz_j(tr_idx));
-
-O_tr(tr_id) = 1;
-O_tr = O_tr == 1;
-O_tst = (O - O_tr) == 1;
+% tr_idx = randsample(nnz(O), floor(tr_fraction * nnz(O)));
+% tr_id=sub2ind(size(O),nnz_i(tr_idx),nnz_j(tr_idx));
+% 
+% O_tr(tr_id) = 1;
+% O_tr = O_tr == 1;
+% O_tst = (O - O_tr) == 1;
 
 hyperpara.n = n;
 hyperpara.d = d;
 hyperpara.L = L;
 hyperpara.h = h;
-hyperpara.iv = 1000;
+hyperpara.iv = 1000000;
 
 %% Setting hyperparameters
 hyperpara.m_mu = 0;
@@ -74,7 +74,7 @@ hyperpara.b0 = 10*sqrt(10) /2;
 hyperpara.m_b0 = [-6 -2 2 6];
 hyperpara.v0 = 0.1;
 
-max_iter = 1;   %iteration number
+max_iter = 10;   %iteration number
 
 %% Initialization should be here
 para = init_para(O_tr, hyperpara);
